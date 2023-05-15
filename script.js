27529bdb62cd48e2fc7e3d157af011e03f154bf9
AbbyMicah <132540488+AbbyMicah@users.noreply.github.com>
2023-05-15 02:57:39 +0200

    Showing current date and time

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+function formatDate(){
+let date = new Date();
+let hours=date.getHours();
+        if(hours<10){
+            hours = `0${hours}`;
+        }
+        let minutes=date.getMinutes();
+        if(minutes<10){
+            minutes = `0${minutes}`;
+        }
+  let dayIndex=date.getDay();
+        let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        let day=days[dayIndex];
+        return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
     console.log(response)  ;
     let temperature = Math.round(response.data.temperature.current);
     let temperatureElement = document.querySelector("#temperature");
     let description = document.querySelector("#temperature-description");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = `${temperature}ºC`;
     description.innerHTML = response.data.condition.description;
     document.querySelector("#city").innerHTML = response.data.city;
     document.querySelector("#humidity").innerHTML = response.data.temperature.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#temp").innerHTML = Math.round(response.data.temperature.feels_like);
-    
+    dateElement.innerHTML = formatDate(response.time);
   }
   let apiKey = "0dbe3edd0f2e43o0a1f5a4d8b80t5af4";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Bilbao&key=${apiKey}&units=metric`;
